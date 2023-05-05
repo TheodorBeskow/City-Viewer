f84c8d7fd4e7be1285a6f4b33e946003e5f0db2f
TheodorBeskow <92083686+TheodorBeskow@users.noreply.github.com>
2023-05-05 17:28:30 +0200

    Can now visualize all countries at the same time

--- Scraper/Scripts/VisualizeData.py
+++ Scraper/Scripts/VisualizeData.py
@@ -1,25 +1,37 @@
 import matplotlib.pyplot  as plt
+import glob
 import json
   
 # take in the desired country code
 cc = input("Country code: ").upper()
-f = open('./Countries/'+cc+'.json', 'r', encoding='utf8')
-
   
-# returns JSON object as a dictionary
-cities = json.load(f)
-print(f"Amount of cities in country {cc} = {len(cities)}")
-
 XList = []
 YList = []
 
-# Adding all the coordinates of the cities to XList and YList
-for city in cities:
-    XList.append(city["longitude"])
-    YList.append(city["latitude"])
-  
-# Closing json file
-f.close()
+# Function to add all cities from a country
+def addCoordinates(countryCode):
+    # Find json file
+    try:
+        f = open('./Countries/'+countryCode+'.json', 'r', encoding='utf8')
+    except:
+        print("There are no country with country code name", countryCode)
+        quit()
+
+    # Returns JSON object as a dictionary
+    cities = json.load(f)
+
+    # Adding all the coordinates of the cities to XList and YList
+    for city in cities:
+        XList.append(city["longitude"])
+        YList.append(city["latitude"])
+    
+    # Closing json file
+    f.close()
+
+if cc == "ALL":
+    for file in glob.glob("./Countries/*"):
+        addCoordinates(file[-7:-5])
+else: addCoordinates(cc)
 
 # Showing UI
 plt.close("all")
